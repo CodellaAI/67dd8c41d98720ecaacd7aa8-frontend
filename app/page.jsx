d07b42c0799f207bb3921b41dcd8fbcc8c1a86e6
EdwardBelt <75886512+EdwardBelt@users.noreply.github.com>
2025-03-21 17:07:03 +0100

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -31,7 +31,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
         <h1 className="text-2xl font-bold text-center text-gray-800">
-          Simple Click Logger
+          Simple Click Loggeraaaaaaaa
         </h1>
         
         <div className="flex flex-col items-center">
